dd900e83c8f461080c9b90e71804e876bb27c402
sez3 <sez_@gmx.at>
2014-01-12 12:05:15 -0800

    issues client

--- Client/src/main/java/at/ac/tuwien/swa/Client.java
+++ Client/src/main/java/at/ac/tuwien/swa/Client.java
@@ -1,62 +1,60 @@
 package at.ac.tuwien.swa;
 
-import java.io.IOException;
-import javax.swing.UIManager;
-import java.util.logging.Logger;
 import java.awt.Dimension;
+import java.util.logging.Logger;
+
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 /**
- * The client console app.
- * Created by grumpo on 12/7/13.
+ * The client console app. Created by grumpo on 12/7/13.
  */
-
 
 public class Client extends JFrame {
 
 	private final static Logger logger = Logger.getLogger(Client.class.getName());
 	private static ServerConnection server = new ServerConnection();
-	
+
 	private static final long serialVersionUID = 1L;
 
 	public Client() {
-		
-        JFrame frame = new JFrame("SWAzam Desktop Client");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        Dimension frameDimension = new Dimension(984, 800);
-        
-        frame.setPreferredSize(frameDimension);
-        frame.setMinimumSize(frameDimension);
-        frame.setMaximumSize(frameDimension);
-        
-        new LoginPanel(frame, server);
-        
-    	frame.pack();
-        frame.setVisible(true);
-        
+
+		JFrame frame = new JFrame("SWAzam Desktop Client");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Dimension frameDimension = new Dimension(984, 800);
+
+		frame.setPreferredSize(frameDimension);
+		frame.setMinimumSize(frameDimension);
+		frame.setMaximumSize(frameDimension);
+
+		new LoginPanel(frame, server);
+
+		frame.pack();
+		frame.setVisible(true);
+
 	}
-	
+
 	public static void main(String[] args) {
 
-	    logger.info("Client starting...");
+		logger.info("Client starting...");
 
 		try {
-			UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
-	    } catch (Exception e) {
-	            e.printStackTrace();
-	    }
+			// UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+			// dont work on MacOS
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-            	@SuppressWarnings("unused")
+				@SuppressWarnings("unused")
 				JFrame frame = new Client();
-            }
-        });
-		
+			}
+		});
+
 		logger.info("Client shutting down...");
 
 	}
--- Client/src/main/java/at/ac/tuwien/swa/LoginPanel.java
+++ Client/src/main/java/at/ac/tuwien/swa/LoginPanel.java
@@ -19,11 +19,10 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
 
-
 public class LoginPanel extends JPanel {
-	
+
 	private static final long serialVersionUID = 5008526318031780589L;
-	
+
 	private static JPanel titlePanel;
 	private static JLabel labelSwazam;
 	private static JButton loginButton;
@@ -31,16 +30,16 @@ public class LoginPanel extends JPanel {
 	private static JTextField usernameField;
 	private static JLabel passwordLabel;
 	private static JPasswordField passwordField;
-	
+
 	public LoginPanel(final JFrame frame, final ServerConnection server) {
-		
+
 		final Container pane = frame.getContentPane();
-		
+
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints cs = new GridBagConstraints();
 
 		cs.fill = GridBagConstraints.HORIZONTAL;
-		
+
 		titlePanel = new JPanel(new GridBagLayout());
 		labelSwazam = new JLabel("SWAzam");
 		labelSwazam.setFont(new Font("Agency FB", Font.PLAIN, 48));
@@ -50,7 +49,7 @@ public class LoginPanel extends JPanel {
 		cs.gridwidth = 3;
 		titlePanel.add(labelSwazam);
 		pane.add(titlePanel, BorderLayout.PAGE_START);
-		
+
 		loginButton = new JButton("Log in");
 
 		usernameLabel = new JLabel("Username: ");
@@ -76,41 +75,45 @@ public class LoginPanel extends JPanel {
 		cs.gridy = 1;
 		cs.gridwidth = 2;
 		passwordField.addKeyListener(new KeyListener() {
-			
+
 			@Override
 			public void keyTyped(KeyEvent arg0) {
-				if(arg0.getKeyChar() == KeyEvent.VK_ENTER) {
+				if (arg0.getKeyChar() == KeyEvent.VK_ENTER) {
 					loginButton.doClick();
 				}
 			}
-			
+
 			@Override
-			public void keyReleased(KeyEvent arg0) { }
-			
+			public void keyReleased(KeyEvent arg0) {
+			}
+
 			@Override
-			public void keyPressed(KeyEvent arg0) { }
+			public void keyPressed(KeyEvent arg0) {
+			}
 		});
-		
+
 		this.add(passwordField, cs);
 		this.setBorder(new LineBorder(Color.GRAY));
 
 		final JPanel bp = new JPanel();
-		
+
 		loginButton.addActionListener(new ActionListener() {
 
+			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (server.verifyLogin(usernameField.getText().trim(), passwordField.getPassword())) {
-					
+
 					pane.removeAll();
-					
-					new MainPanel(frame, server);					
-					
+
+					new MainPanel(frame, server);
+
 					pane.repaint();
-					
+					pane.revalidate();
+
 				}
 			}
 		});
-		
+
 		bp.add(loginButton);
 		pane.add(this, BorderLayout.CENTER);
 		pane.add(bp, BorderLayout.PAGE_END);
